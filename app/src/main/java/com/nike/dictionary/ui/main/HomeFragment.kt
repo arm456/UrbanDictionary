4929f389c815b9ce4711402f91ed4ead18bfa69f
arm456 <allam.risha@gmail.com>
2019-12-15 20:34:18 -0500

    Updated with dynamic search of string typed in SearchView

--- app/src/main/java/com/nike/dictionary/ui/main/HomeFragment.kt
+++ app/src/main/java/com/nike/dictionary/ui/main/HomeFragment.kt
@@ -11,9 +11,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
-import com.nike.dictionary.R
 import com.nike.dictionary.databinding.ActivityWordsListBinding
 import com.nike.dictionary.ui.viewmodel.LoadDictionaryWordListViewModel
+import androidx.appcompat.widget.SearchView
+import com.nike.dictionary.R
+
 
 class HomeFragment : Fragment() {
 
@@ -37,16 +39,33 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.setContentView(requireActivity(), R.layout.activity_words_list)
         binding.wordsList.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
 
-        var searchTerm: String="home"
         viewModel = ViewModelProviders.of(this).get(LoadDictionaryWordListViewModel::class.java)
-
-        viewModel.loadWordLists(searchTerm)
+        configureSearchViewForDynamicStringSearch()
         viewModel.errorMessage.observe(this, Observer {
                 errorMessage -> if(errorMessage != null) showError(errorMessage) else hideError()
         })
         binding.viewModel = viewModel
     }
 
+    private fun configureSearchViewForDynamicStringSearch(){
+        binding.search.isActivated = true
+        binding.search.queryHint = getString(R.string.query_hint)
+        binding.search.onActionViewExpanded()
+        binding.search.isIconified = false
+        binding.search.clearFocus()
+
+        binding.search.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(searchTerm: String): Boolean {
+                viewModel.loadWordLists(searchTerm)
+                return false
+            }
+        })
+    }
     private fun showError(@StringRes errorMessage:Int){
         errorSnackbar = Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_INDEFINITE)
         errorSnackbar?.setAction(R.string.retry, viewModel.errorClickListener)
